6a8c2be4264c28202068127be463d2dad7e61b65
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2022-01-05 18:02:56 +0530

    Starting with C

---
+++ C_Tutorials/add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() /* Execution of program start from here*/
+{
+    int a , b;
+    printf("Enter Number a \n");
+    scanf("%d",&a);
+
+    printf("Enter Number b \n");
+    scanf("%d",&b);
+
+    printf("The sum is %d \n", a+b);
+    return 0;
+
+}
---
+++ C_Tutorials/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h> /* prprocessor command , It is a header File*/
+
+int main() // Function 
+{
+    
+    printf("Hello World\n");
+    
+    return 0;
+}
+
