502edd63cbf4ae18b04851529dff3e3995b870be
Pankaj(GoSu) <Pankajgoswami330@gmail.com>
2022-01-15 21:05:45 +0530

    Files

---
+++ C_Tutorials/Tut62_File_I/O.c
@@ -0,0 +1,24 @@
+/*
+Author : Pankaj Goswami
+Purpose : File I/O in C.
+Date : 15/01/2022
+*/
+
+/*
+=>>> Why Do We Need a File?
+Ans - Files are used to store data and information.
+    Files are used to read & access data anytime from the hard disk.
+    Files make it easy for a programmer to access and store content without
+    losing it on program termination.
+
+=> Types Of Files
+1- Text Files.
+2- Binary Files. (More Secure)
+
+=>> File Operations In C.
+In C we can perform these high level operation on files.
+1- Creating a new file 
+2- Opening a file
+3- Closing a file
+4- Reading from and writing to a file.
+*/
